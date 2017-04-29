660fd7ce86ac7683c24b262af2ebd269b88d4f7f
Rohit Yarlagadda <rohityarlagadda@Rohits-MacBook-Pro.local>
2017-04-28 23:09:09 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by rohityarlagadda on 4/28/17.
+ */
+public class HelloWorld {
+    public static void hello(){ // Static Method
+        System.out.println("I am from static method");
+    }
+    public void hello1(){  //Simple Method
+        System.out.println("I am from instance/simple method");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by rohityarlagadda on 4/28/17.
+ */
+
+public class Main {
+    public static void main(String[] args){
+
+        HelloWorld.hello();//Static Method
+        //HelloWorld.hello1();// X
+        HelloWorld a= new HelloWorld();
+        a.hello1();
+        }
+}
